5252d92d763fc32182c470e96a825b624cb881fe
Janessa Tran <tranjanessa@gmail.com>
2020-07-08 14:51:29 -0700

    Initial Commit

---
+++ MorningBird/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MorningBird
+//
+//  Created by Janessa Tran on 7/8/20.
+//  Copyright © 2020 Janessa Tran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
